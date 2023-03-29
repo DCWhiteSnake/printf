6d07d19b924ffd4dabad54b24544ca48cb3a14a2
Vikiey <ohaivicky00@gmail.com>
2023-03-28 23:55:17 -0700

    printf

--- main.h
+++ main.h
@@ -4,6 +4,10 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 void write_number(int n);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_u(va_list u);
+int print_o(va_list o);
 int printstr(char *c);
 void writenum(int n);
 #endif
